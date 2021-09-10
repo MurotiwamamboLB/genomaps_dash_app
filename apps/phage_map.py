8623e55374ef32761e7d295eded3b1879ee9cc50
MurotiwamamboLB <murotiwamambo@locus-bio.com>
2021-09-10 13:46:32 -0400

    The current link is now working. This will help people to know exactly where they are. The solution was to chage the className of menu links in the index.py. When current, the classname changes of a a class that is styled as current link by css.

--- apps/phage_map.py
+++ apps/phage_map.py
@@ -3,8 +3,10 @@ import base64
 #import datetime
 import io
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
+# import dash_core_components as dcc
+# import dash_html_components as html
 # import plotly.express as px
 # import pandas as pd
 from app import app
