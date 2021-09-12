4fe42dbed8320a4e9ac692c53ac59273933305e0
MurotiwamamboLB <murotiwamambo@locus-bio.com>
2021-09-12 02:41:10 -0400

    Corrected wrong spelings

--- apps/home.py
+++ apps/home.py
@@ -11,10 +11,10 @@ layout = html.Div(children=[
    html.Div([
 
 	html.H2(["Welcome to the engineering plasmid and phage maps generator!"]),
-    html.P(["Generate annotated engineering plasmid maps."]),
-    html.P(["Generate annotated engineered phage maps."]),
-    html.P(["All input and out is in the GenBank file format."]),
-    html.P(["Download your results real time."]),
+    html.H6(["Generate annotated engineering plasmid maps."]),
+    html.H6(["Generate annotated engineered phage maps."]),
+    html.H6(["All inputs and outputs are in the GenBank file format."]),
+    html.H6(["Download your results in real time."]),
     ], className = "body-text"),
 
    # body block ends
